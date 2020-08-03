259fc791248c14efa976ff91afab1ab49646b513
Chahak Mehta <201501422@daiict.ac.in>
2020-08-03 13:00:35 +0530

    Add support for Life 1.05 files

--- src/quadlife.py
+++ src/quadlife.py
@@ -1,13 +1,18 @@
 from functools import lru_cache
 from .utils import read_pattern, Node, get_dead, get_alive
+import time
 
 
 def play(filename, iterations):
-    points = read_pattern(filename)
+    points, desc = read_pattern(filename)
     root_node = _list_to_tree(points)
+    start_t = time.time()
     result = _play_game(root_node, iterations)
+    end_t = time.time()
+    time_taken = end_t - start_t
+    # all_nodes_points = [_tree_to_list(node) for node in all_nodes]
     result_points = _tree_to_list(result)
-    return result_points
+    return points, result_points, time_taken
 
 
 def _play_game(node, iterations=0):
--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,81 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def read_pattern(filename):
+    root, ext = os.path.splitext(filename)
+    if ext == '.txt':
+        return _read_txt(filename)
+    elif ext == '.lif' or ext == '.life':
+        return _read_lif(filename)
+    else:
+        raise Exception("File type provided as input is not supported. Please provide a .txt or .lif/.life file")
+
+def _read_txt(filename):
     with open(filename, "r") as f:
         lines = f.readlines()
+    description = []
     positions = []
     for i, line in enumerate(lines):
         for j, char in enumerate(line):
             if char == "1":
                 positions.append((j, i))
-    return positions
+    return positions, description
+
+def _read_lif(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    description = []
+    positions = []
+    origin = [0, 0]
+
+    for line in lines:
+        if line[0] == '#':
+            if line.startswith('#Life'):
+                continue
+            elif line[1] == 'D':
+                if len(description) <= 22:
+                    description.append(line[2:].strip())
+                else:
+                    raise Exception("LIF/LIFE file should not have more than 22 #D lines. Please refer https://www.conwaylife.com/wiki/Life_1.05 for more information about the format")
+            elif line[1] == "R":
+                raise Exception("Rules in LIF not supported in this version. The game runs on the standard rules for now.")
+            elif line[1] == "P":
+                point_coords = [int(x) for x in line[2:].strip().split()]
+                origin = [-point_coords[0], -point_coords[1]]
+                y = origin[1]
+            else:
+                raise Exception(f"# should be followed by D/R/P. Found: {line}")
+        else:
+            if len(line) > 0:
+                for x, c in enumerate(line):
+                    if c == '*':
+                        positions.append((x + origin[0], y + origin[1]))
+                y += 1
+
+    description = '\n'.join(description)
+    return positions, description
+
+def plot_points(pattern, result, title="Game of Life", output_image_name='outputs/output.png'):
+    cmap = np.zeros((200,200))
+    cmap_original = np.zeros((200,200))
+    for x, y in pattern:
+        cmap_original[y+cmap_original.shape[0]//2, x+cmap_original.shape[1]//2] = 1
+
+    for x, y in result:
+        cmap[y+cmap.shape[0]//2, x+cmap.shape[1]//2] = 1
 
 
-def plot_points(points, title="Game of Life", output_image_name='outputs/output.png'):
-    x_max = max([x for x, y in points]) + 5
-    y_max = max([y for x, y in points]) + 5
+    # from pprint import pprint
+    # pprint(cmap)
 
-    cmap = np.zeros((y_max, x_max))
-    for x, y in points:
-        cmap[y][x] = 1
-
-    fig, ax = plt.subplots()
-    ax.imshow(cmap)
-    ax.set_title(title)
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(cmap_original)
+    ax[0].set_title("Original")
+    ax[1].imshow(cmap)
+    ax[1].set_title(title)
     plt.savefig(output_image_name, dpi=450)
     return ax, output_image_name
 
